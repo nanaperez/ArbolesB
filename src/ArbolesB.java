cbe2f06b7c314d0039225b11a118c24ee6ba3474
jessecogollo <cogollo87@gmail.com>
2013-11-02 10:31:42 -0500

    Configurando entorno desarrollo 2

---
+++ src/ArbolesB.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jessecogollo
+ * Date: 2/11/13
+ * Time: 10:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArbolesB {
+
+    public static void main(String  [] misArgumentos) {
+
+        System.out.println("ArbolesB");
+    }
+}
